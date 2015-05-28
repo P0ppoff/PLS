0d05810106205f35ec34f67735fb34c3ce3c4ba0
mouretad <adriengoal@hotmail.fr>
2015-05-28 10:59:22 +0200

    ajout initialisation dico

--- gestion_donnees.c
+++ gestion_donnees.c
@@ -1,1 +1,19 @@
+#include "gestion_donnees.h"
+#include "types.h"
 
+void init_dico (dico *dictionnaire){
+	int i;
+	cellule *cellule_precedente = NULL;
+	cellule *cellule_courante;
+	dictionnaire = creer_cellule();
+	cellule_courante = dictionnaire;
+	for (i = 0; i < 258; i++)
+	{
+		cellule_courante -> index = i;
+		cellule_courante -> elt = i;
+		cellule_courante -> frere_precedent = cellule_precedente;
+		cellule_courante -> frere_suivant = creer_cellule();
+		cellule_precedente = cellule_courante;
+		cellule_courante = cellule_courante -> frere_suivant;
+	}
+}
