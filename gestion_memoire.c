e74ad56c407ef6eeb7cc5ca9b52648928c89d27c
MatthieuCrouzet <mattcrouzet@hotmail.fr>
2015-05-28 09:48:17 +0200

    gestion de la memoire

--- gestion_memoire.c
+++ gestion_memoire.c
@@ -1,1 +1,47 @@
 
+void liberer_dico (dico * ptr_dico){
+    free(ptr_dico);
+    ptr_dico = NULL;
+}
+
+void liberer_sequence (sequence * ptr_seq){
+    free(ptr_seq);
+    ptr_seq = NULL;
+}
+
+void liberer_cellule (cellule * ptr_cell){
+    free(ptr_cell);
+    ptr_cell = NULL;
+}
+
+
+dico * creer_dico {
+    dico * ptr_dico = malloc(sizeof(dico));
+    if (ptr_dico == NULL) {
+        printf("Allocation dictionnaire impossible\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr_dico;
+}
+
+sequence * creer_sequence {
+    sequence * ptr_seq = malloc(sizeof(sequence));
+    if (ptr_seq == NULL) {
+        printf("Allocation sequence impossible\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr_seq;
+}
+
+cellue * creer_cellule {
+    cellule * ptr_cellule = malloc(sizeof(cellule));
+    if (ptr_cellule == NULL) {
+        printf("Allocation cellule impossible\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr_cellule;
+}
+
+void modif_lg_table (dico[] table){
+    table[0] = (dico *) realloc(table[0],2*sizeof(table));
+}
--- gestion_memoire.h
+++ gestion_memoire.h
@@ -1,6 +1,9 @@
 #ifndef _GESTION_MEMOIRE_H_
 #define _GESTION_MEMOIRE_H_
 
+#include <stdlib.h>
+#include "types.h"
+
 void liberer_dico (dico * dic);
 
 dico * creer_dico;
