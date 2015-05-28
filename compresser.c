83f9ba6ecbf1c7d1981e7ae6ee7dd7660dd0788b
P0ppoff <jules.hablot@gmail.com>
2015-05-28 14:50:43 +0200

    amélioration de la compression

--- compresser.c
+++ compresser.c
@@ -1,27 +1,30 @@
 #include "compresser.h"
 #include "types.h"
 
+#define TAILLE_LU 8
+
 void compresser(FILE* f_in, FILE* f_out){
+	int TAILLE_ECRIT = 9; // on commence sur 9 pour avoir deux infos de plus : Fin de fichier & incrémentation du nombre de bits à écrire
+	int INDICE_MAX = 257; // dernier indice donné
 	dico *dictionnaire;
 	cellule *cell_w = NULL;
 	cellule *cell_s = NULL;
-	init_compression(dictionnaire);
-	sequence *w, *s, *a;
+	sequence *w, *a;
 
-	w = lecture_bits(f_in,8);
-	while(!est_fin(w)){ 
-		a = lecture_bits(f_in,8);
-		cell_w = rechercher_dico(w,dictionnaire);
-		cell_s = rechercher_freres(a,cell_w);
-		if(cell_s == NULL){
-			s = ajout_queue(w,a); // création de la sequence à rechercher
-			w = s;
+	init_compression(dictionnaire);
+	w = lecture_bits(f_in, TAILLE_LU);
+	while(!est_fin(w)){ // vérification w  = dernier élément à tous les bits à 1 = 255
+		a = lecture_bits(f_in, TAILLE_LU);
+		cell_w = rechercher_dico(w, dictionnaire); 
+		cell_s = rechercher_fils(a, cell_w); // pramatère : le parent
+		if(cell_s != NULL){ // w.a appartient au dictionnaire
+			w = ajout_queue(w,a); // création de la sequence à rechercher
 		}else{
-			ecriture_indice(cell_w,f_out);
-			ajout_element(a,cell_w);
-			w = {NULL,a};
+			ecriture_indice(cell_w, f_out, TAILLE_ECRIT);
+			ajout_element(a, cell_w, &TAILLE_ECRIT, &INDICE_MAX);
+			w = {a, NULL};
 		}
 	}
-	ecriture_fin(cell_w,f_out); // fichier vide ou pas 
+	ecriture_fin(cell_w, f_out, TAILLE_ECRIT); // fichier vide ou pas 
 }
 
