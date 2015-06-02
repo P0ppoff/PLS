484255b5fb9e72df42db89808b37327b65ccb899
mouretad <adriengoal@hotmail.fr>
2015-06-02 14:11:06 +0200

    modif decompression

--- decompresser.c
+++ decompresser.c
@@ -1,5 +1,11 @@
 #include "decompresser.h"
 
+
+void init_tampon_lecturee(tampon *t){
+	t -> nb_bits_dispo = 0;
+	t -> buffer = 0;
+}
+
 void decompresser(FILE* f_in, FILE* f_out){
 	int TAILLE_LU = 9; // on commence sur 9 pour avoir deux infos de plus : Fin de fichier & incrémentation du nombre de bits à écrire
 	int INDICE_MAX = 257; // dernier indice donné
@@ -19,14 +25,15 @@ void decompresser(FILE* f_in, FILE* f_out){
 	i = creer_sequence();
 	a = creer_sequence();
 	w = creer_sequence();
+	iprime = creer_sequence();
 	wprime = creer_sequence();
+	init_tampon_lecturee(&bl);
 	lecture_bits(f_in, TAILLE_LU, &bl, i);
 	indice = conversion(i, TAILLE_LU);
 	recupere_seq(table, indice, a); // renvoie la séquence pour aboutir à la case d'index i // antention à la fin de fichier, si table[i]==NULL soit fin de fichier -> a = EOF soit increment
 	extraction_tete(a, w); // transfère dans la séquence w
 	ecriture_seq(f_out, w); // nous n'utilisons pas de buffer d'écriture car nous écrivons sur un octet -> amélioration = passer par un buffer pour faire des écriture sur plus de 8 bits
 	while(!est_fin_fichier(f_in)){ // vérification w  = dernier élément à tous les bits à 1 = 255
-		iprime = creer_sequence();
 		lecture_bits(f_in, TAILLE_LU, &bl, iprime);
 		indice = conversion(iprime, TAILLE_LU); // conversion d'une séquence en l'entier correspondant
 		if(table[indice].racine == NULL){
